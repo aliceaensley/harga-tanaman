58bacc0d9206c044168aaef18d95aafd78db044a
aliceaensley <ainsleyalice6@gmail.com>
2025-10-17 09:15:31 +0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,7 @@
 -- FiveM Client-Side Lua Script
 Citizen.CreateThread(function()
     while true do
-        -- Update setiap 50 milidetik (20 FPS)
+        -- Update setiap 50 milidetik
         Citizen.Wait(50) 
         
         local playerPed = PlayerPedId()
@@ -12,16 +12,15 @@ Citizen.CreateThread(function()
             
             -- Mendapatkan Data dari Game menggunakan FiveM Natives
             local speed_mps = GetEntitySpeed(vehicle)
-            local speed_kmh = math.floor(speed_mps * 3.6) -- Konversi ke KM/H
+            local speed_kmh = math.floor(speed_mps * 3.6) -- KM/H
             local rpm = GetVehicleCurrentRpm(vehicle)      -- RPM (0.0 sampai 1.0)
             local gear = GetVehicleCurrentGear(vehicle)    -- Gear saat ini
             local fuel = math.floor(GetVehicleFuelLevel(vehicle)) -- Fuel level (0-100)
-            -- Engine Health normalnya 1000, dibagi 10 untuk 0-100%
             local engineHealth = math.floor(GetVehicleEngineHealth(vehicle) / 10) 
 
-            -- Mengirim Data ke NUI melalui Event
+            -- Mengirim Data ke NUI yang di-load dari URL Eksternal
             SendNUIMessage({
-                type = 'updateSpeedometer',
+                type = 'updateSpeedometer', -- Kunci event, ASUMSI cocok dengan JS eksternal
                 inVehicle = true,
                 speed = speed_kmh,
                 rpm = rpm, 
@@ -34,7 +33,6 @@ Citizen.CreateThread(function()
             SendNUIMessage({
                 type = 'updateSpeedometer',
                 inVehicle = false,
-                -- Data tetap dikirim agar JS bisa menerima inVehicle=false
             })
         end
     end
